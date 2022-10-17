f65fe2e9440164e12a99199428aaa3b59ef5ef83
Márcio Santos <saturno17.mv@gmail.com>
2022-10-16 23:17:00 -0300

    Trabalhando com propriedades.

--- src/App.js
+++ src/App.js
@@ -5,8 +5,13 @@ function App() {
   return (
     <div className="App">
       <h1> Meu projeto</h1>
-      <Comentario/>
-      <Comentario/>
+      <Comentario nome="João" email="joao@mail.com" data={new Date(2022, 10, 16)}>
+        Olá, tudo bem?
+      </Comentario>
+      <Comentario nome="Maria" email="maria@mail.com" data={new Date(2022, 10, 16)}>
+        Olá, tudo sim.
+      </Comentario>
+
     </div>
   );
 }
