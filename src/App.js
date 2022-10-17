fc2b6f7e4ef94002fb06085e26c9458a558dde9e
Márcio Santos <saturno17.mv@gmail.com>
2022-10-16 23:36:55 -0300

    Criando o estado da aplicação.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
+import React, { Component } from 'react';
 import './App.css';
 import Comentario from './components/Comentario';
 
-function App() {
-  return (
-    <div className="App">
-      <h1> Meu projeto</h1>
-      <Comentario nome="João" email="joao@mail.com" data={new Date(2022, 10, 16)}>
-        Olá, tudo bem?
-      </Comentario>
-      <Comentario nome="Maria" email="maria@mail.com" data={new Date(2022, 10, 16)}>
-        Olá, tudo sim.
-      </Comentario>
+class App extends Component {
 
-    </div>
-  );
+  state = {
+    comentarios: [
+      {
+        nome: 'João',
+        email: 'joao@mail.com',
+        data: new Date(2022, 10, 16),
+        mensagem: 'Olá, tudo bem?'
+      },
+      {
+        nome: 'Maria',
+        email: 'maria@mail.com',
+        data: new Date(2022, 10, 16),
+        mensagem: 'Olá, tudo sim'
+      }
+    ]
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1> Meu projeto</h1>
+        {this.state.comentarios.map((comentario, indice) => (
+          <Comentario
+            key={indice}
+            nome={comentario.nome}
+            email={comentario.email}
+            data={comentario.data}>
+            {comentario.mensagem}
+          </Comentario>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
